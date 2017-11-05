edb67c509905feb09640ca240d2c73b847bd7d84
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-11-05 23:34:37 +0530

    Create Data Service

---
+++ src/services/data-server.js
@@ -0,0 +1,27 @@
+let instance = null;
+var wishList = [];
+
+class DataService {
+  constructor() {
+    if (!instance) {
+      instance = this;
+    }
+
+    return instance;
+  }
+
+  addWishListItem = item => {
+    wishList.push(item);
+  }
+
+  removeWishListItem = item => {
+    for (var i = 0; i < wishList.length; i++) {
+      if (wishList[i]._id === item._id) {
+        wishList.splice(x, 1);
+        break;
+      }
+    }
+  }
+}
+
+export default DataService;
